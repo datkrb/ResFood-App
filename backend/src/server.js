912bd34b587ff655c7833a2ad48234a6988e0c4f
cuogne <cuog.nc116@gmail.com>
2026-01-29 10:38:21 +0700

    add ngrok

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
+const ngrok = require('@ngrok/ngrok');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
+    // use ngrok in package npm 
+    ngrok.connect({
+        addr: PORT,
+        authtoken_from_env: true
+    })
+        .then(listener => console.log(`Ingress established at: ${listener.url()}`))
+        .catch(err => console.error('Error establishing ngrok tunnel:', err));
 });
